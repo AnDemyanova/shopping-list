fc76eedffa1b917661c2bbb5951ae6ecb1310862
AnDemyanova <anna.demyanova96@gmail.com>
2025-03-06 16:16:38 +0300

    Домашнее задание 4

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const input = document.querySelector('#input');
+const items = document.querySelector('.items');
+
+input.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+
+        const groceryText = input.value;
+
+        const newPoint = document.createElement('div');
+        newPoint.classList.add('point');
+        newPoint.textContent = groceryText;
+
+        if (groceryText != '') {
+            items.append(newPoint);
+        }
+
+        input.value = '';
+    }
+})
+
+for (const item of groceryText) {
+    item.addEventListener('click', function() {
+        item.classList.toggle('decoration');
+    })
+}
 
 /* Пустые элементы не должны добавляться */
 
